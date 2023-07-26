9803c21499cbb089b57dafb96f4a3293090c2563
aaron deik <aarondeik@live.cl>
2023-07-26 18:13:51 -0400

    funciona el daterangepicker y esta con estilo

--- app/javascript/controllers/daterangepicker_controller.js
+++ app/javascript/controllers/daterangepicker_controller.js
@@ -3,6 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    new DateRangePicker(this.element)
+    new DateRangePicker(this.element, {
+      alwaysShowCalendars: true,
+      autoApply: true,
+      ranges: {
+          'Today': [moment().startOf('day'), moment().endOf('day')],
+          'Yesterday': [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')],
+          'Last 7 Days': [moment().subtract(6, 'days').startOf('day'), moment().endOf('day')],
+          'This Month': [moment().startOf('month').startOf('day'), moment().endOf('month').endOf('day')],
+      },
+    })
   }
 }
