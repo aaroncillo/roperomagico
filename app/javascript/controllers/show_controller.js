c23116362e227cbefceedf56e3538153e456c88e
aaron deik <aarondeik@live.cl>
2023-08-13 22:10:42 -0400

    funciona bien los filtros

--- app/javascript/controllers/show_controller.js
+++ app/javascript/controllers/show_controller.js
@@ -1,29 +1,28 @@
-// app/javascript/controllers/show_controller.js
 import { Controller } from 'stimulus';
 import debounce from "debounce";
 
 export default class extends Controller {
-  connect() {
-      document.addEventListener('DOMContentLoaded', function() {
-        const filterButtons = document.querySelectorAll('.filter-button');
-        const filterInputs = document.querySelectorAll('.filter-input');
+  static targets = ['filterButton', 'filterInput', 'phoneFilterInput', 'phoneFilterButton', 'RutFilterInput', 'RutFilterButton'];
 
-        filterButtons.forEach((button, index) => {
-          button.addEventListener('click', function() {
-            filterInputs.forEach((input, inputIndex) => {
-              if (inputIndex === index) {
-                input.classList.toggle('hidden');
-              } else {
-                input.classList.add('hidden');
-              }
-            });
-          });
-        });
-      });
-  }
   initialize() {
     this.submit = debounce(this.submit.bind(this), 300);
   }
+
+  toggleFilterInput(event) {
+    const index = this.filterButtonTargets.indexOf(event.target);
+    this.filterInputTargets[index].classList.toggle('hidden');
+  }
+
+  togglePhoneFilterInput(event) {
+    const index = this.phoneFilterButtonTargets.indexOf(event.target);
+    this.phoneFilterInputTargets[index].classList.toggle('hidden');
+  }
+
+  toggleRutFilterInput(event) {
+    const index = this.RutFilterButtonTargets.indexOf(event.target);
+    this.RutFilterInputTargets[index].classList.toggle('hidden');
+  }
+
   submit() {
     this.element.requestSubmit();
   }
