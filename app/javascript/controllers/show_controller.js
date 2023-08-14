c3550fbf857b82f3d4946c63e1e1d37e64b17313
aaron deik <aarondeik@live.cl>
2023-08-13 21:06:01 -0400

    fasdf

--- app/javascript/controllers/show_controller.js
+++ app/javascript/controllers/show_controller.js
@@ -4,22 +4,22 @@ import debounce from "debounce";
 
 export default class extends Controller {
   connect() {
-    document.addEventListener('DOMContentLoaded', function() {
-      const filterButtons = document.querySelectorAll('.filter-button');
-      const filterInputs = document.querySelectorAll('.filter-input');
+      document.addEventListener('DOMContentLoaded', function() {
+        const filterButtons = document.querySelectorAll('.filter-button');
+        const filterInputs = document.querySelectorAll('.filter-input');
 
-      filterButtons.forEach((button, index) => {
-        button.addEventListener('click', function() {
-          filterInputs.forEach((input, inputIndex) => {
-            if (inputIndex === index) {
-              input.classList.toggle('hidden');
-            } else {
-              input.classList.add('hidden');
-            }
+        filterButtons.forEach((button, index) => {
+          button.addEventListener('click', function() {
+            filterInputs.forEach((input, inputIndex) => {
+              if (inputIndex === index) {
+                input.classList.toggle('hidden');
+              } else {
+                input.classList.add('hidden');
+              }
+            });
           });
         });
       });
-    });
   }
   initialize() {
     this.submit = debounce(this.submit.bind(this), 300);
